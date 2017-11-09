13cbb471e4d6fff3ec187199279eed34fd29a347
Eric Reyes <eric.reyes@datarobot.com>
2017-11-08 22:02:59 -0600

    Resize karel

--- karelmap.py
+++ karelmap.py
@@ -1105,6 +1105,7 @@ class Ui_MainWindow(object):
         for square in all_squares:
             square.setPixmap(pixmap)
 
+
     def set_walls(self):
         pass
 
@@ -1130,14 +1131,12 @@ class Ui_MainWindow(object):
 
         karel_program = self.textEdit.toPlainText()
         check_lex_and_syntax(karel_program)
+        print('reading form board')
         return karel_program
 
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
-
-
-
         self.textEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
             "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
             "p, li { white-space: pre-wrap; }\n"
